8f18dc9ca5c586f9812100f21f2af712d591bde6
giacobenin <giacomo.benincasa@gmail.com>
2015-02-15 20:20:30 -0500

    Minor changes.

--- src/test/java/edu/ufl/cise/cnt5106c/conf/CommonPropertiesTest.java
+++ src/test/java/edu/ufl/cise/cnt5106c/conf/CommonPropertiesTest.java
@@ -1,7 +1,5 @@
 package edu.ufl.cise.cnt5106c.conf;
 
-import edu.ufl.cise.cnt5106c.conf.CommonProperties;
-import edu.ufl.cise.cnt5106c.conf.CommonProperties;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map.Entry;
