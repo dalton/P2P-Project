1609f112f8e1aa21575f6d6cde0bb811f6ad7ec4
giacobenin <giacomo.benincasa@gmail.com>
2015-04-19 18:35:51 -0400

    Added chockedPeers (Collection<Integer> chokedPeersIds) and unchockedPeers (Collection<Integer> unchokedPeersIds) callback methods to notify listener that the sets of choked and unchoked neighbors have changed.

--- src/main/java/edu/ufl/cise/cnt5106c/PeerManagerListener.java
+++ src/main/java/edu/ufl/cise/cnt5106c/PeerManagerListener.java
@@ -1,9 +1,14 @@
 package edu.ufl.cise.cnt5106c;
 
+import java.util.Collection;
+
 /**
  *
  * @author Giacomo Benincasa    (giacomo@cise.ufl.edu)
  */
 public interface PeerManagerListener {
     public void neighborsCompletedDownload();
+
+    public void chockedPeers (Collection<Integer> chokedPeersIds);
+    public void unchockedPeers (Collection<Integer> unchokedPeersIds);
 }
