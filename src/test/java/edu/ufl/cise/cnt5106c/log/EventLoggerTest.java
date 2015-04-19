2409e32ef000ab334efabc38f6b2ba177202d182
giacobenin <giacomo.benincasa@gmail.com>
2015-04-19 16:49:07 -0400

    Minor changes.

--- src/test/java/edu/ufl/cise/cnt5106c/log/EventLoggerTest.java
+++ src/test/java/edu/ufl/cise/cnt5106c/log/EventLoggerTest.java
@@ -32,31 +32,8 @@ public class EventLoggerTest {
      */
     @Test
     public void testAll(){
-        String peerConnectionMsg1 = "1 makes a connection to Peer 2";
-        assertEquals("Peer 1 must be making connection", peerConnectionMsg1, );
-        String peerConnectionMsg2 = "1 is connected from Peer 2";
-        assertEquals("Peer 1 must be connected", peerConnectionMsg2, );
-
-        String chokeMessage = "1 is choked by 2";
-        assertEquals("Peer 1 must be choked by 2", chokeMessage, );
-
-        String unchokeMessage = "1 is unchoked by 2";
-        assertEquals("Peer 1 must be unchoked by 2", unchokeMessage, );
-
-        String haveMessage = "1 received the 'have' message from 2 for the piece 1.";
-        assertEquals("Peer 1 must receive the 'have' message from 2 for the piece 1", haveMessage, );
-
-        String interestedMessage = "1 received the 'interested' message from 2.";
-        assertEquals("Peer 1 must receive the 'interested' message from 2", interestedMessage, );
-
-        String notInterestedMessage = "1 received the 'not interested' message from 2.";
-        assertEquals("Peer 1 must receive the 'not interested' message from 2", notInterestedMessage, );
-
-        String pieceDownloadedMessage = "1 has downloaded the piece 1 from peer 2. Now the number of pieces it has is 1.";
-        assertEquals("Peer 1 must downloade the piece 1 from peer 2. Now the number of pieces it will have is 1", pieceDownloadedMessage, );
-
-        String fileDownloadedMessage = "1 has downloaded the complete file";
-        assertEquals("Peer 1 must download the complete file", fileDownloadedMessage, );
+        LogHelper.getLogger().info("somthinjpogjrehy[ot");
+        
     }
 
 }
