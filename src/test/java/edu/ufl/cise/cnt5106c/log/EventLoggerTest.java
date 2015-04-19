ebe7921a64b66d4721a5e4de1aae1c5bf3cfe728
ssingh8008 <ssingh8008@gmail.com>
2015-04-19 16:01:07 -0400

    Added configuration for LogHelper

--- src/test/java/edu/ufl/cise/cnt5106c/log/EventLoggerTest.java
+++ src/test/java/edu/ufl/cise/cnt5106c/log/EventLoggerTest.java
@@ -29,41 +29,73 @@ public class EventLoggerTest {
     /**
      * Test of peerConnection method, of class EventLogger.
      */
+    @Test
+    public void testPeerConnection(){
 
-
+    }
 
     /**
      * Test of chokeMessage method, of class EventLogger.
      */
+    @Test
+    public void testChokeMessage(){
+
+    }
 
     /**
      * Test of unchokeMessage method, of class EventLogger.
      */
+    @Test
+    public void testUnchokeMessage(){
+
+    }
 
     /**
      * Test of haveMessage method, of class EventLogger.
      */
+    @Test
+    public void testHaveMessage(){
+
+    }
 
     /**
      * Test of interestedMessage method, of class EventLogger.
      */
+    @Test
+    public void testInterestedMessage(){
+
+    }
 
     /**
      * Test of notInterestedMessage method, of class EventLogger.
      */
+    @Test
+    public void testNotInterestedMessage(){
+
+    }
 
     /**
      * Test of pieceDownloadedMessage method, of class EventLogger.
      */
+    @Test
+    public void testPieceDownloadedMessage(){
+
+    }
 
     /**
      * Test of fileDownloadedMessage method, of class EventLogger.
      */
+    @Test
+    public void testFileDownloadedMessage(){
+
+    }
 
     /**
      * Test of getLogMsgHeader method, of class EventLogger.
      */
+    @Test
+    public void testGetLogMsgHeader(){
 
-
+    }
 
 }
