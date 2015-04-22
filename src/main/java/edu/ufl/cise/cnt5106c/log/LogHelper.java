b9079c98234d37d27f184a056b81e070e3b16b04
giacobenin <giacomo.benincasa@gmail.com>
2015-04-22 18:43:18 -0400

    Changed debugging level.

--- src/main/java/edu/ufl/cise/cnt5106c/log/LogHelper.java
+++ src/main/java/edu/ufl/cise/cnt5106c/log/LogHelper.java
@@ -97,11 +97,11 @@ public class LogHelper {
     }
 
     public synchronized void debug (String msg) {
-        _l.log(Level.FINE, msg);
+        _l.log(Level.SEVERE, msg);
     }
 
     public synchronized void info (String msg) {
-        _l.log (Level.INFO, msg);
+        _l.log (Level.SEVERE, msg);
     }
 
     public synchronized void severe (String msg) {
@@ -109,7 +109,7 @@ public class LogHelper {
     }
 
     public synchronized void warning (String msg) {
-        _l.log(Level.WARNING, msg);
+        _l.log(Level.SEVERE, msg);
     }
 
     public synchronized void severe (Throwable e) {
