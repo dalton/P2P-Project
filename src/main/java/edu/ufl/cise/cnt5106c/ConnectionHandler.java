703f97b0459e44434299eabc12b56082615464d4
Adam Dalton <adalton@ihmc.us>
2015-04-22 22:02:29 -0400

    nll pointer

--- src/main/java/edu/ufl/cise/cnt5106c/ConnectionHandler.java
+++ src/main/java/edu/ufl/cise/cnt5106c/ConnectionHandler.java
@@ -7,6 +7,7 @@ import edu.ufl.cise.cnt5106c.log.EventLogger;
 import edu.ufl.cise.cnt5106c.messages.Handshake;
 import edu.ufl.cise.cnt5106c.messages.Message;
 import edu.ufl.cise.cnt5106c.messages.Request;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
@@ -14,7 +15,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- *
  * @author Giacomo Benincasa    (giacomo@cise.ufl.edu)
  */
 public class ConnectionHandler implements Runnable {
@@ -31,36 +31,36 @@ public class ConnectionHandler implements Runnable {
     private final AtomicInteger _remotePeerId;
     private final BlockingQueue<Message> _queue = new LinkedBlockingQueue<>();
 
-    public ConnectionHandler (int localPeerId, Socket socket, FileManager fileMgr, PeerManager peerMgr)
+    public ConnectionHandler(int localPeerId, Socket socket, FileManager fileMgr, PeerManager peerMgr)
             throws IOException {
-        this (localPeerId, false, -1, socket, fileMgr, peerMgr);
+        this(localPeerId, false, -1, socket, fileMgr, peerMgr);
     }
 
-    public ConnectionHandler (int localPeerId, boolean isConnectingPeer, int expectedRemotePeerId,
-            Socket socket, FileManager fileMgr, PeerManager peerMgr) throws IOException {
+    public ConnectionHandler(int localPeerId, boolean isConnectingPeer, int expectedRemotePeerId,
+                             Socket socket, FileManager fileMgr, PeerManager peerMgr) throws IOException {
         _socket = socket;
         _localPeerId = localPeerId;
         _isConnectingPeer = isConnectingPeer;
         _expectedRemotePeerId = expectedRemotePeerId;
         _fileMgr = fileMgr;
         _peerMgr = peerMgr;
-        _out = new ProtocolazibleObjectOutputStream (_socket.getOutputStream());
-        _remotePeerId = new AtomicInteger (PEER_ID_UNSET);
+        _out = new ProtocolazibleObjectOutputStream(_socket.getOutputStream());
+        _remotePeerId = new AtomicInteger(PEER_ID_UNSET);
     }
 
-    public int getRemotePeerId(){
+    public int getRemotePeerId() {
         return _remotePeerId.get();
     }
 
     @Override
     public void run() {
-        new Thread () {
+        new Thread() {
 
             private boolean _remotePeerIsChoked = true;
 
             @Override
             public void run() {
-                Thread.currentThread().setName (getClass().getName() + "-" + _remotePeerId + "-sending thread");
+                Thread.currentThread().setName(getClass().getName() + "-" + _remotePeerId + "-sending thread");
                 while (true) {
                     try {
                         final Message message = _queue.take();
@@ -72,7 +72,7 @@ public class ConnectionHandler implements Runnable {
                                 case Choke: {
                                     if (!_remotePeerIsChoked) {
                                         _remotePeerIsChoked = true;
-                                        sendInternal (message);
+                                        sendInternal(message);
                                     }
                                     break;
                                 }
@@ -80,43 +80,40 @@ public class ConnectionHandler implements Runnable {
                                 case Unchoke: {
                                     if (_remotePeerIsChoked) {
                                         _remotePeerIsChoked = false;
-                                        sendInternal (message);
+                                        sendInternal(message);
                                     }
                                     break;
                                 }
 
                                 default:
-                                    sendInternal (message);
+                                    sendInternal(message);
                             }
-                        }
-                        else {
+                        } else {
                             LogHelper.getLogger().debug("cannot send message of type "
                                     + message.getType() + " because the remote peer has not handshaked yet.");
                         }
-                    }
-                    catch (IOException ex) {
+                    } catch (IOException ex) {
                         LogHelper.getLogger().warning(ex);
-                    }
-                    catch (InterruptedException ex) {
+                    } catch (InterruptedException ex) {
                     }
                 }
             }
         }.start();
 
         try {
-            final ProtocolazibleObjectInputStream in = new ProtocolazibleObjectInputStream (_socket.getInputStream());
+            final ProtocolazibleObjectInputStream in = new ProtocolazibleObjectInputStream(_socket.getInputStream());
 
             // Send handshake
-            _out.writeObject (new Handshake (_localPeerId));
+            _out.writeObject(new Handshake(_localPeerId));
 
             // Receive and check handshake
             Handshake rcvdHandshake = (Handshake) in.readObject();
-            _remotePeerId.set (rcvdHandshake.getPeerId());
-            Thread.currentThread().setName (getClass().getName() + "-" + _remotePeerId.get());
-            final EventLogger eventLogger = new EventLogger (_localPeerId);
-            final MessageHandler msgHandler = new MessageHandler (_remotePeerId.get(), _fileMgr, _peerMgr, eventLogger);
+            _remotePeerId.set(rcvdHandshake.getPeerId());
+            Thread.currentThread().setName(getClass().getName() + "-" + _remotePeerId.get());
+            final EventLogger eventLogger = new EventLogger(_localPeerId);
+            final MessageHandler msgHandler = new MessageHandler(_remotePeerId.get(), _fileMgr, _peerMgr, eventLogger);
             if (_isConnectingPeer && (_remotePeerId.get() != _expectedRemotePeerId)) {
-                throw new Exception ("Remote peer id " + _remotePeerId + " does not match with the expected id: " + _expectedRemotePeerId);
+                throw new Exception("Remote peer id " + _remotePeerId + " does not match with the expected id: " + _expectedRemotePeerId);
             }
 
             // Handshake successful
@@ -125,27 +122,26 @@ public class ConnectionHandler implements Runnable {
             sendInternal(msgHandler.handle(rcvdHandshake));
             while (true) {
                 try {
-                    sendInternal (msgHandler.handle ((Message) in.readObject()));
-                }
-                catch (Exception ex) {
-                    LogHelper.getLogger().warning (ex);
+                    sendInternal(msgHandler.handle((Message) in.readObject()));
+                } catch (Exception ex) {
+                    LogHelper.getLogger().warning(ex);
                     break;
                 }
             }
+        } catch (Exception ex) {
+            LogHelper.getLogger().warning(ex);
+        } finally {
+            try {
+                _socket.close();
+            } catch (Exception e) {
+            }
         }
-        catch (Exception ex) {
-            LogHelper.getLogger().warning (ex);
-        }
-        finally {
-            try { _socket.close(); }
-            catch (Exception e) {}
-        }
-        LogHelper.getLogger().warning (Thread.currentThread().getName()
-                    + " terminating, messages will no longer be accepted.");
+        LogHelper.getLogger().warning(Thread.currentThread().getName()
+                + " terminating, messages will no longer be accepted.");
     }
 
     @Override
-    public boolean equals (Object obj) {
+    public boolean equals(Object obj) {
         if (obj instanceof ConnectionHandler) {
             return ((ConnectionHandler) obj)._remotePeerId == _remotePeerId;
         }
@@ -159,21 +155,21 @@ public class ConnectionHandler implements Runnable {
         return hash;
     }
 
-    public void send (final Message message) {
+    public void send(final Message message) {
         _queue.add(message);
     }
 
-    private synchronized void sendInternal (Message message) throws IOException {
+    private synchronized void sendInternal(Message message) throws IOException {
         if (message != null) {
-            _out.writeObject (message);
-        }
-        switch (message.getType()) {
-            case Request: {
-                new java.util.Timer().schedule(
-                    new RequestTimer ((Request) message, _fileMgr, _queue, _remotePeerId.get()), 
-                    _peerMgr.getUnchokingInterval() * 2
-                );
+            _out.writeObject(message);
+            switch (message.getType()) {
+                case Request: {
+                    new java.util.Timer().schedule(
+                            new RequestTimer((Request) message, _fileMgr, _queue, _remotePeerId.get()),
+                            _peerMgr.getUnchokingInterval() * 2
+                    );
+                }
             }
-        }        
+        }
     }
 }
